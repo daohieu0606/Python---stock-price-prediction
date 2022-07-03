84b2cc0dcf9a65181af94e3456fb4c7c11c46352
ngocchi111 <18120010@student.hcmus.edu.vn>
2022-07-04 03:01:25 +0700

    RNN & XGBoost

---
+++ RNN.py
@@ -0,0 +1,102 @@
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Dense, Dropout, LSTM, RNN
+from tensorflow.keras import layers
+from tensorflow.keras import backend
+from tensorflow import keras
+import datetime as dt
+import pandas_datareader as web 
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+prediction_days = 60;
+start = dt.datetime(2015, 1,1)
+end = dt.datetime(2022, 1,1 )
+
+#load data
+company = 'META'
+data = web.DataReader(company, 'yahoo', start, end)
+
+class MinimalRNNCell(keras.layers.Layer):
+
+    def __init__(self, units, **kwargs):
+        self.units = units
+        self.state_size = units
+        super(MinimalRNNCell, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(shape=(input_shape[-1], self.units),
+                                      initializer='uniform',
+                                      name='kernel')
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units),
+            initializer='uniform',
+            name='recurrent_kernel')
+        self.built = True
+
+    def call(self, inputs, states):
+        prev_output = states[0]
+        h = backend.dot(inputs, self.kernel)
+        output = h + backend.dot(prev_output, self.recurrent_kernel)
+        return output, [output]
+
+# Let's use this cell in a RNN layer:
+
+
+def getModel():
+    # prepare data
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+    x_train = []
+    y_train = []
+
+    for x in range(prediction_days, len(scaled_data)):
+        x_train.append(scaled_data[x - prediction_days: x, 0])
+        y_train.append(scaled_data[x, 0])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    #Build model
+
+    model = Sequential()
+    #model.add(layers.Embedding(input_dim=1000, output_dim=64))
+
+    # The output of GRU will be a 3D tensor of shape (batch_size, timesteps, 256)
+    model.add(layers.GRU(256, return_sequences=True))
+
+    # The output of SimpleRNN will be a 2D tensor of shape (batch_size, 128)
+    model.add(layers.SimpleRNN(36))
+
+    model.add(layers.Dense(10))
+
+    model.compile(optimizer='adam', loss ='mean_squared_error')
+    model.fit(x_train, y_train, epochs = 5, batch_size = 32)
+
+    return model;
+
+def predictTestData(model, test_data):
+    total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days: ].values
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.transform(model_inputs)
+
+    #Make prediction on test data
+
+    x_test = []
+
+    for x in range(prediction_days, len(model_inputs)):
+        x_test.append(model_inputs[x - prediction_days: x, 0])
+
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    predicted_prices = model.predict(x_test)
+    predicted_prices = scaler.inverse_transform(predicted_prices)
+
+    predicted_prices = predicted_prices.reshape(-1,)
+    
+    return predicted_prices
---
+++ XGBoost.py
@@ -0,0 +1,62 @@
+
+import datetime as dt
+import pandas_datareader as web 
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_boston
+from sklearn.preprocessing import LabelEncoder
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+prediction_days = 60;
+start = dt.datetime(2015, 1,1)
+end = dt.datetime(2022, 1,1 )
+
+#load data
+company = 'META'
+data = web.DataReader(company, 'yahoo', start, end)
+
+def getModel():
+    # prepare data
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+    x_train = []
+    y_train = []
+
+    for x in range(prediction_days, len(scaled_data)):
+        x_train.append(scaled_data[x - prediction_days: x, 0])
+        y_train.append(scaled_data[x, 0])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    #x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    le = LabelEncoder()
+    y_train = le.fit_transform(y_train)
+
+    #Build model
+
+    model = XGBClassifier()
+    model.fit(x_train, y_train)
+    return model
+
+def predictTestData(model, test_data):
+
+    total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days: ].values
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.transform(model_inputs)
+ 
+    #Make prediction on test data
+
+    x_test = []
+    for x in range(prediction_days, len(model_inputs)):
+        x_test.append(model_inputs[x - prediction_days: x, 0])
+    x_test = np.array(x_test)
+
+    predicted_prices = model.predict(x_test)
+    return predicted_prices
